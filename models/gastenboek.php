bf41be08471c1e69213d1c89b25bfb558fc9ad8e
Mathias Zabot <mathias.zabot@gmail.com>
2019-03-29 20:34:46 +0100

    gastenboek

---
+++ models/gastenboek.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mathias
+ * Date: 29/03/2019
+ * Time: 20:31
+ */
+
+class gastenboek extends db
+{
+    public $id,$title,$text;
+
+    public function addData($table = null,$naam =null,$bericht=null){
+        $stmt = $this->conn->prepare("INSERT INTO ".$table." (naam, fbericht)
+        VALUES ('".$naam."','".$bericht."')");
+        $stmt->execute();
+    }
+    public function deleteDatabyId($table = null,$id = null){
+        $stmt = $this->conn->prepare("DELETE FROM ".$table." WHERE id=".$id);
+        $stmt->execute();
+    }
+
+    public function updateDatabyId($table = null, $naam=null, $bericht=null, $id = null) {
+        $stmt = $this->conn->prepare("UPDATE ".$table." SET naam = '".$naam."', bericht = '".$bericht."' WHERE id= ".$id);
+        $stmt->execute();
+    }
+}
